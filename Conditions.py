9d88ba5da5e2f107e436ca6a74e229e087fe5a50
goni715 <halesgoni715149123@gmail.com>
2024-10-10 12:45:50 +0600

    if..else conditions

--- Conditions.py
+++ Conditions.py
@@ -32,9 +32,52 @@ b = 10
 if a > b: print("a is greater than b")
 
 
+
 # Short Hand If ... Else
 # One line if else statement:
-# 
+# This technique is known as Ternary Operators
+#
 print("A") if a > b else print("B")
+# output = A
 
 
+
+# One line if else statement, with 3 conditions:
+x = 330
+y = 330
+
+print("A") if x > y else print("Equal") if x == y else print("B")
+# output: Equal
+
+
+
+
+
+# using logical operator = and or not
+#
+# and operator
+p = 200
+q = 33
+r = 500
+if p > q and r > p:
+  print("Both conditions are True")
+
+
+# or operator
+if p > q or p > r:
+  print("At least one of the conditions is True")
+
+
+# using not operator
+m = 33
+n = 200
+if not m > n:
+  print("m is NOT greater than n")
+
+
+
+# The pass Statement
+# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+#
+if n > m:
+  pass
