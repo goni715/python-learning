af986b97642c80feffd2b805e7789df402bad6c0
goni715 <halesgoni715149123@gmail.com>
2024-10-10 11:50:48 +0600

    python conditions

---
+++ Conditions.py
@@ -0,0 +1,9 @@
+# Python Conditions and If statements
+#
+# Following conditions can be used in several ways, most commonly in "if statements" and loops.
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
