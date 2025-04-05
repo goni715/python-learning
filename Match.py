d28001a9e27adb94cf35fb1cae40730139da21a2
goni715 <halesgoni715149123@gmail.com>
2025-04-05 13:08:51 +0600

    Python match

---
+++ Match.py
@@ -0,0 +1,28 @@
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+#Default Value
+# Use the underscore character _ as the last case value if you want a code block to execute when there are not other matches
+
+day = 4
+match day:
+  case 6:
+    print("Today is Saturday")
+  case 7:
+    print("Today is Sunday")
+  case _:
+    print("Looking forward to the Weekend")
