1f127ec30edd1cb328f7204c2b49d90cc32d0728
goni715 <halesgoni715149123@gmail.com>
2025-04-20 11:56:44 +0600

    arbitrary arguments

--- Function.py
+++ Function.py
@@ -20,3 +20,15 @@ my_function2("Osman")
 # arguments are the actual values passed to the function when it is called.
 # here, arguments = "Osman"
 
+
+
+#Arbitrary Arguments, *args
+# If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+# I will receive tuple as params for arbitrary function.
+
+def my_function3(*kids):
+  # print(kids)  ('Evan', 'Marjan', 'Bishwas')== this is tuple
+  print("The youngest child is " + kids[0]) #output = The youngest child is Evan
+
+my_function3("Evan", "Marjan", "Bishwas")
+
