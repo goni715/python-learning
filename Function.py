1eb4f95c0da371454018879f54cc17ede88f1c0a
goni715 <halesgoni715149123@gmail.com>
2025-04-20 11:49:40 +0600

    python function

---
+++ Function.py
@@ -0,0 +1,22 @@
+print("This is Function File");
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+
+def my_function2(fname):
+  print(fname + " Goni")
+
+my_function2("Osman")
+
+
+# parameters vs arguments
+# parameters are the named variables
+# here, parameters = fname
+
+# arguments are the actual values passed to the function when it is called.
+# here, arguments = "Osman"
+
